f58321d39a4001b04f40b64560809b1b5468a699
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2019-04-04 13:45:52 -0400

    The test creates a new file.

--- HangmanGame/HangmanTests.py
+++ HangmanGame/HangmanTests.py
@@ -1,29 +1,35 @@
 #File: HangmanTests.py
 
 from HangmanFunctions import *
+from os import remove
 from os import system
 
 def wordReadingTest():
-    wordList = readWordFile("words.txt")
+    fileName = "wordsForTest.txt"
+    wordsForTest = open(fileName, "w")
+    wordsForTest.write("chose\nessai\nmot\ntest")
+    wordsForTest.close()
+    wordList = readWordFile(fileName)
+    remove(fileName)
     word = str()
     try:
         word = wordList[0]
-        assert(word=="armoire")
+        assert(word=="chose")
     except AssertionError:
         print("Error: wordList[0] = {0}".format(word))
     try:
         word = wordList[1]
-        assert(word=="boucle")
+        assert(word=="essaie")
     except AssertionError:
         print("Error: wordList[1] = {0}".format(word))
     try:
         word = wordList[2]
-        assert(word=="buisson")
+        assert(word=="mot")
     except AssertionError:
         print("Error: wordList[2] = {0}".format(word))
     try:
         word = wordList[len(wordList)-1]
-        assert(word=="volant")
+        assert(word=="test")
     except AssertionError:
         print("Error: last word list = {0}".format(word))
 
